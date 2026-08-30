d298af8ea2faa782df2d7baedf07f29296a07a1a
yasinozyapi8 <yasinozyapi8@gmail.com>
2026-08-30 18:16:14 +0300

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("com.android.application") version "8.12.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
-}
